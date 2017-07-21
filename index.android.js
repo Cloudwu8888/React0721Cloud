833814de0f0f0dcb0096ffb8a20f6ae44c45bf43
Cloudwu8888 <cloudsunday@foxmail.com>
2017-07-21 01:34:47 -0700

    AddpushcodeandChangeButton

--- index.android.js
+++ index.android.js
@@ -6,8 +6,10 @@
 import MobileCenter from 'mobile-center';
 import crashes from 'mobile-center-crashes';
 import analytics from 'mobile-center-analytics';
+import push from 'mobile-center-push';
 import React, { Component } from 'react';
 import {
+  Button,
   Alert,
   AppState,
   AppRegistry,
@@ -60,6 +62,34 @@ export default class react0721Cloud extends Component {
     );
   }
 }
+Push.setEventListener({
+  pushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
+
+    if (message === null || message === undefined) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = "Android background";
+      message = "<empty>";
+    }
+
+    // Custom name/value pairs set in the Mobile Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+      // In this case you'll want to save off the notification info and wait until the app is fully shown
+      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+      // when the app is fully in the foreground.
+    }
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -79,5 +109,6 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
 
 AppRegistry.registerComponent('react0721Cloud', () => react0721Cloud);
