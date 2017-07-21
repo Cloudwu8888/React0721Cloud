1cbffdb203e415f00b2db58a7d875fd8bd9360b4
Cloudwu8888 <cloudsunday@foxmail.com>
2017-07-21 00:13:08 -0700

    AddcrashesCode

--- index.android.js
+++ index.android.js
@@ -3,20 +3,50 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import MobileCenter from 'mobile-center';
+import crashes from 'mobile-center-crashes';
+import analytics from 'mobile-center-analytics';
 import React, { Component } from 'react';
 import {
+  Alert,
+  AppState,
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
+Analytics.trackEvent("my first event");
+Analytics.trackEvent("my second event");
+function ClickBtn(){
+  alert("Welcome");
+}
+function ChangeDisable(){
+  this.setState({
+    disabled:this.state.disabled ?false:true
+  });
+  throw new Error("This is a test javascript crash!");
+}
 export default class react0721Cloud extends Component {
+  constructor(props){
+    super(props);
+    this.state={disabled:false,}
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Button
+                 onPress={ClickBtn}
+                 title="Learn More"
+                 color="green"
+                 disabled={this.state.disabled}
+                 accessibilityLabel="Learn more about this purple button"/>
+        <Button 
+                onPress={ChangeDisable.bind(this)}
+                title="change"
+                color="gray"
+                accessibilityLabel="Learn more about this purple button"/>
+
+       /* <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
@@ -25,7 +55,7 @@ export default class react0721Cloud extends Component {
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
-        </Text>
+        </Text>*/
       </View>
     );
   }
